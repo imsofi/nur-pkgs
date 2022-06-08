4c0cd4b37dbd6f678775c388050a3a7f2fa299d8
Sofi <sofi+git@mailbox.org>
2022-06-08 18:40:58 +0200

    flask-webtest: init at 0.0.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3,4 +3,6 @@ rec {
   authlib1 = callPackage ../development/python-modules/authlib1 { };
 
   flask-themer = callPackage ../development/python-modules/flask-themer { };
+
+  flask-webtest = callPackage ../development/python-modules/flask-webtest { };
 }
